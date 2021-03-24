8b71e2e5fdbfb52cc6309a349df6dff6c77074d7
gherrerar <gabi.gh597@gmail.com>
2021-03-23 21:07:50 -0300

    Atualização Final v8

--- server/config.js
+++ server/config.js
@@ -7,7 +7,11 @@ const io = require('socket.io')(http);
 
 var clients = {};
 
-app.get('/', (req, res,) => {
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'chat.html'));
+});
+app.get('/allow-cors', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
   res.sendFile(path.join(__dirname, '..', 'public', 'chat.html'));
 });
 app.use(express.static("../public"));
